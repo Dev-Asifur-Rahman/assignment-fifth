eae898c7230be00a91f5d3f5992eab89018c5fad
Dev-Asifur-Rahman <asifurrahman.ac@gmail.com>
2024-02-18 02:44:29 +0600

    slight

--- jsfile/index.js
+++ jsfile/index.js
@@ -1,5 +1,10 @@
-const a = 3;
+/*const a = 3;
 const c = document.getElementById('my_modal_5');
 if (a == 2){
     c.classList.remove('hidden');
+}
+*/
+function navigate_ticket_form(){
+    const a = document.getElementById('ticket_form');
+    a.scrollIntoView({behavior:'smooth'});
 }
