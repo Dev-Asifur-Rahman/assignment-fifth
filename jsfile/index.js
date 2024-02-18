98391c1cc70aa10b82ea72321c82e0869e6726be
Dev-Asifur-Rahman <asifurrahman.ac@gmail.com>
2024-02-18 14:44:04 +0600

    des done

--- jsfile/index.js
+++ jsfile/index.js
@@ -1,9 +1,3 @@
-/*const a = 3;
-const c = document.getElementById('my_modal_5');
-if (a == 2){
-    c.classList.remove('hidden');
-}
-*/
 function navigate_ticket_form(){
     const a = document.getElementById('ticket_form');
     a.scrollIntoView({behavior:'smooth'});
